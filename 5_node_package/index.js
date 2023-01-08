55bc70fad3bb79eae1486325a14b8a492b145766
hasanmehedi9034 <mehedi90344@gmail.com>
2023-01-08 20:54:11 +0600

    semantic versioning

--- 5_node_package/index.js
+++ 5_node_package/index.js
@@ -8,6 +8,16 @@ const http = require('http');
 
 
 
+// "react": "^17.0.1"
+// major.minor.patch => semantic versioning
+
+// 1.0.0 => 1.0.1 => 1.0.2 --patch update
+// 1.1.0 => add feature(minor versin)
+// 2.0.0 => major changed
+// ^ -> caret (install latest without changing major version)
+// ~ -> tilda (install patch latest)
+// ^16.1.5 => 16.x
+// ~16.1.5 => 16.1.x
 
 
 
