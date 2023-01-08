875c14c0b26a3e495bf691f1d4fa2418bb441af5
hasanmehedi9034 <mehedi90344@gmail.com>
2023-01-08 15:53:11 +0600

    event create and trigger by different file

--- 4_built_in_node_module/event.js
+++ 4_built_in_node_module/event.js
@@ -1,19 +1,25 @@
 // event module
 
-const EventEmitter = require('events');
-const emmiter = new EventEmitter();
+const MyEvent = require('./myEvent');
+const myEvent = new MyEvent();
 
 // create event and there work
-emmiter.on('event1', (e) => {
-    console.log(e)
-})
+// emmiter.on('event1', (e) => {
+//     console.log(e)
+// })
 
 // create event and there work
-emmiter.on('event2', () => {
-    console.log('Event 2 Called')
-})
+// emmiter.on('event2', () => {
+//     console.log('Event 2 Called')
+// })
 
 // call event
-emmiter.emit('event1', { name: 'Mehedi', id: 1 })
+// emmiter.emit('event1', { name: 'Mehedi', id: 1 })
+
+myEvent.on('event1', () => {
+    console.log('Event 1 Triggered');
+})
+
+myEvent.function1()
 
 
