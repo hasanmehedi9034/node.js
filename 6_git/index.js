ff5f5682f087ffd53c4cbd50777381c3514fd64f
hasanmehedi9034 <mehedi90344@gmail.com>
2023-01-09 19:28:46 +0600

    basic of version control system with git

---
+++ 6_git/index.js
@@ -0,0 +1,24 @@
+/*
+setup git:
+    git config --global user.name 'username'
+    git config --global user.email 'email'
+    git config --list (for showing detail with git enviornment)
+
+repo:
+    git init
+    git status
+    git log or git log --oneline
+    git checkout commitnumber filename
+    git checkout master (for detached head)
+    git reset filename (for unstage a file)
+
+onlie rep:
+    showing details in website
+    git push -u origin master
+
+----------------------------------------------------------------
+ignore a file or folder:
+    create a file which name is .gitignore
+    and write filename or folder name, which you want to ignore
+
+*/
