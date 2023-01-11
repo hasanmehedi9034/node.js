622788113ed46553957e2b07775ec09d890e58b7
hasanmehedi9034 <mehedi90344@gmail.com>
2023-01-11 15:42:14 +0600

    asynchronus with call back

--- 8_call_back/1_async.js
+++ 8_call_back/1_async.js
@@ -1,14 +1,18 @@
+// callback function
+
 console.log('Line 1');
 
-const students = getStudent(1);
-console.log(students);
+getStudent(2, (student) => { 
+    console.log(student);
+});
+
 
 console.log('Line 2');
 
-function getStudent(id) {
+function getStudent(id, cb) {
     setTimeout(() => {
-        console.log('fetching from database');
-        return {id: id, name: 'Rahim'};
+        console.log('fetching from database...');
+        cb({ id: id, name: 'Rahim' });
     }, 2000)
 }
 
