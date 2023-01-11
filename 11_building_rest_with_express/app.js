2c482f4b9d4a13cd68f4ba3a04065515cfecdbe2
hasanmehedi9034 <mehedi90344@gmail.com>
2023-01-11 23:40:26 +0600

    create a server with express and install nodemon

--- 11_building_rest_with_express/app.js
+++ 11_building_rest_with_express/app.js
@@ -1,15 +1,37 @@
 // Express
 
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.write("Hello world");
-        res.end();
-    }
-});
+// const server = http.createServer((req, res) => {
+//     if (req.url === '/') {
+//         res.write("Hello world");
+//         res.end();
+//     }
+// });
 
-server.listen(3000);
+// server.listen(3000);
 
-console.log('Listening on port 300...')
+// console.log('Listening on port 300...')
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+    response.send("Hello from express js!");
+})
+
+app.get('/another', (request, response) => {
+    response.send("I am another response")
+})
+
+app.get('/student', (request, response) => {
+    response.send(JSON.stringify(['Mehedi', 'Hasan']))
+})
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
 
