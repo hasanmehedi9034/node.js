f17cfaeefe56f25bd569ad248c746124719f4825
hasanmehedi9034 <mehedi90344@gmail.com>
2023-01-11 16:00:09 +0600

    callback hell

--- 8_call_back/1_async.js
+++ 8_call_back/1_async.js
@@ -4,6 +4,12 @@ console.log('Line 1');
 
 getStudent(2, (student) => { 
     console.log(student);
+    getCourses(student, (courses) => {
+        console.log(courses);
+        getQuizmarks(courses.courses, (marks)=> {
+            console.log(marks);
+        })
+    })  
 });
 
 
@@ -14,5 +20,26 @@ function getStudent(id, cb) {
         console.log('fetching from database...');
         cb({ id: id, name: 'Rahim' });
     }, 2000)
+}
+
+function getCourses(student, cb) {
+    setTimeout(() => {
+        console.log("Student courses from database");
+        cb({
+            id: student.id, 
+            name: student.name,
+            courses: ['js', 'python']
+        });
+    }, 1500)
+}
+
+function getQuizmarks(courses, cb) {
+    setTimeout(() => {
+        console.log('Student quizes');
+        cb({ 
+            [courses[0]]: 5,
+            [courses[1]]: 10,
+        })
+    }, 1000)
 }
 
