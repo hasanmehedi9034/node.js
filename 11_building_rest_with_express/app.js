651730047feb1e1e78e8d126f6b70697bafc4c15
hasanmehedi9034 <mehedi90344@gmail.com>
2023-01-12 00:22:57 +0600

    post data from clients

--- 11_building_rest_with_express/app.js
+++ 11_building_rest_with_express/app.js
@@ -17,6 +17,7 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
+app.use(express.json())
 
 app.get('/', (request, response) => {
     response.send("Hello from express js!");
@@ -26,6 +27,17 @@ app.get('/api/students', (req, res) => {
     fs.readFile('./db.json', 'utf-8', (err, data) => {
         const students = JSON.parse(data).students;
         res.send(students);
+    })
+})
+
+app.post('/api/students', (req, res) => {
+    const student = req.body;
+    fs.readFile('./db.json', 'utf-8', (err, data) => {
+        const students = JSON.parse(data)
+        students.students.push(student);
+       fs.writeFile('./db.json', JSON.stringify(students), (err) => {
+            res.send(students);
+       })
     })
 })
 
