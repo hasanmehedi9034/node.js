489a72010e1a60b689950057cf6cea8a53a5ee42
hasanmehedi9034 <mehedi90344@gmail.com>
2023-01-11 23:30:41 +0600

    install express

---
+++ 11_building_rest_with_express/app.js
@@ -0,0 +1,15 @@
+// Express
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write("Hello world");
+        res.end();
+    }
+});
+
+server.listen(3000);
+
+console.log('Listening on port 300...')
+
