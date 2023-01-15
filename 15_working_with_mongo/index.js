8bf1fd89b5d60ba4c66d39193bd602afc2e2ea1d
hasanmehedi9034 <mehedi90344@gmail.com>
2023-01-15 20:26:19 +0600

    create schema of students collection

--- 15_working_with_mongo/index.js
+++ 15_working_with_mongo/index.js
@@ -8,3 +8,18 @@ mongoose.connect('mongodb://127.0.0.1:27017/my-students')
 
 
 
+// Schema -> defines the shape documents
+const studentSchema = new mongoose.Schema({
+    firstName: {type: String},
+    lastName: String,
+    dob: Date,
+    entryDate: {type: Date, default: Date.now},
+    passed: Boolean,
+    hobbies: [String],
+    parents: {
+        father: String,
+        mother: String
+    },
+    subjects: [{name: String, marks: {type: Number, min: 0, max: 100}}]
+}); 
+
