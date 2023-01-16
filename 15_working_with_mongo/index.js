d231f01da249945fafb6fc2c7ba5ea0f8d679e4e
hasanmehedi9034 <mehedi90344@gmail.com>
2023-01-16 15:09:21 +0600

    delete operation

--- 15_working_with_mongo/index.js
+++ 15_working_with_mongo/index.js
@@ -55,15 +55,36 @@ async function createStudent() {
 // Read
 async function readStudents() {
     // const studentData = await Student.find({ firstName: 'Karim' });
-    
+
+    // const studentData = await Student
+    //     .find({ firstName: "Karim" })
+    //     .limit(3)
+    //     .sort({ firstName: 1 })
+    //     .select({ firstName: 1, lastName: 1, hobbies: 1 })
+
     const studentData = await Student
-        .find({ firstName: "Karim" })
-        .limit(3)
-        .sort({ firstName: 1 })
-        .select({ firstName: 1, lastName: 1, hobbies: 1 })
+        .find()
+        .select({ firstName: 1, lastName: 1, passed: 1 })
+        // .countDocuments()
 
     // const studentData = await Student.find();
+
     console.log(studentData);
 }
 readStudents();
+
+async function updateStudent(id) {
+    const student = await Student.updateOne({ __id: id }, {
+        $set: { passed: false }
+    })
+
+    console.log(student);
+}
+//updateStudent('63c5050b8aea503ad121b22d')
+
+async function deleteStudent(id) {
+    const student = await Student.deleteOne({ _id: id })
+    console.log(student);
+}
+// deleteStudent('63c503c4a5c9b7521a7b2542')
 
