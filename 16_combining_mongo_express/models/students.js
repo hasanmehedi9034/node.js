d5df9095f498b74b6f8ee9dc0555a4d8d044ab63
hasanmehedi9034 <mehedi90344@gmail.com>
2023-01-19 16:11:40 +0600

    create student model

---
+++ 16_combining_mongo_express/models/students.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose');
+
+
+const Student = model('Student', Schema({
+    name: { type: String, required: true },
+    age: { type: Number, min: 0 },
+    hobbies: {
+        type: Array,
+        of: String,
+        validate: {
+            validator: (value) =>  value.length > 0,
+            messages: "There must be at least 1 hobby!"
+        }
+    }
+}));
+
+
+exports.Student = Student;
+
+
+
